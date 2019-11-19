ac4e3e27b62bddb1c2395fe7b1efce5090bd9d57
cherenkov <cherenkov@teconpc.local>
2019-11-19 19:21:14 +0300

    project structure

--- src/index.js
+++ src/index.js
@@ -3,45 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF'
-    var color = '#'
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)]
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i += 1) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-    return color
+    return color;
 }
 
 class Box extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name : '',
-            color: getRandomColor()
-        }
-        this.updateName = this.updateName.bind(this)
+            name: '',
+            color: getRandomColor(),
+        };
+        this.updateName = this.updateName.bind(this);
     }
-    
+
     updateName(event) {
         this.setState({
-            name : event.target.value,
-            color: getRandomColor() 
-        })
+            name: event.target.value,
+            color: getRandomColor(),
+        });
     }
 
     render() {
         return (
             <div className="box">
-                <h1 style={{color: this.state.color}}>{this.state.name}</h1>
-                <input type="text" name="nameBox" onChange={this.updateName}/>
+                <h1 style={{ color: this.state.color }}>{this.state.name}</h1>
+                <input type="text" name="nameBox" onChange={this.updateName} />
             </div>
-        )
+        );
     }
 }
 
 ReactDOM.render(
     <React.Fragment>
-        <Box/>
-        <Box/>
+        <Box />
+        <Box />
     </React.Fragment>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
